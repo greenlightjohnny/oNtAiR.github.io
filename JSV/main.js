f3cd7f340b47e7b51c9f765af7b32118cac558b2
jump <39558198+oNtAiR@users.noreply.github.com>
2019-05-30 10:40:10 +0700

    Ugggg

--- JSV/main.js
+++ JSV/main.js
@@ -18,6 +18,7 @@ window.addEventListener('scroll', shrinkNav);
 
 
 
+
 const menuOpen = document.querySelector('.menuOpen');
 const mobileNav = document.querySelector('.mobileNav');
 const slideX = document.querySelector('.slideX');
@@ -29,3 +30,40 @@ function slideOut() {
 
 menuOpen.addEventListener('click', slideOut);
 slideX.addEventListener('click', slideOut);
+
+////////////////////////////////////// Catalog image slider //////////////
+
+const triggerRight = document.querySelectorAll('.attempt .right');
+const triggerLeft = document.querySelectorAll('.attempt .left');
+
+function showNext() {
+  
+  let parent = this.parentNode;
+  let photos = parent.previousElementSibling;
+  
+  
+  
+  ///finds which photo is currently being shown ////////////
+  current = photos.querySelector('.active');
+  
+  
+  /////finds the next sibling img, or the first child if it is the end and ads active ///
+  current.nextElementSibling ? current.nextElementSibling.classList.add('active') :
+  photos.firstElementChild.classList.add('active');
+  current.classList.remove('active');
+  }
+
+function showPrevious() {
+
+  let parent = this.parentNode;
+  let photos = parent.previousElementSibling;
+  current = photos.querySelector('.active');
+
+  current.previousElementSibling ? current.previousElementSibling.classList.add('active') :
+  photos.lastElementChild.classList.add('active');
+  current.classList.remove('active');
+}
+triggerRight.forEach(i => i.addEventListener('click', showNext));
+triggerLeft.forEach(i => i.addEventListener('click', showPrevious));
+
+
